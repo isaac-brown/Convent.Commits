54c038648b1a89c8e77766ac85079ebac4c0ff3d
Isaac Brown <isaac.joseph.brown@gmail.com>
2021-04-04 14:46:34 +1300

    chore: be more specific as to which package to push

--- eng/publish.ps1
+++ eng/publish.ps1
@@ -12,7 +12,8 @@ Set-Location $PSScriptRoot
 
 try {
     Set-Location ..\src\Convent.Commits\bin\Release\
-    dotnet nuget push --source https://api.nuget.org/v3/index.json --api-key $ApiKey
+    dotnet nuget push "*.nupkg" --source https://api.nuget.org/v3/index.json --api-key $ApiKey
+    dotnet nuget push "*.snupkg" --source https://api.nuget.org/v3/index.json --api-key $ApiKey
 }
 finally {
     Pop-Location
