dfc3feee4d4bcc86b8541cc4cd3545710e439fa0
Isaac Brown <isaac.joseph.brown@gmail.com>
2021-04-04 14:52:07 +1300

    chore: stop symbols from being pushed twice

--- eng/publish.ps1
+++ eng/publish.ps1
@@ -13,7 +13,6 @@ Set-Location $PSScriptRoot
 try {
     Set-Location ..\src\Convent.Commits\bin\Release\
     dotnet nuget push "*.nupkg" --source https://api.nuget.org/v3/index.json --api-key $ApiKey
-    dotnet nuget push "*.snupkg" --source https://api.nuget.org/v3/index.json --api-key $ApiKey
 }
 finally {
     Pop-Location
